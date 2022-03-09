d6cdf36d64bfcc16b49a4b78499a0a905640e827
Andrey Miskov <amiskov@gmail.com>
2022-03-09 08:21:55 +0300

    66

--- src/rich4clojure/easy/problem_066.clj
+++ src/rich4clojure/easy/problem_066.clj
@@ -1,5 +1,6 @@
 (ns rich4clojure.easy.problem-066
-  (:require [hyperfiddle.rcf :refer [tests]]))
+  (:require [hyperfiddle.rcf :refer [tests]]
+            [hashp.core]))
 
 ;; = Greatest Common Divisor =
 ;; By 4Clojure user: dbyrne
@@ -8,17 +9,23 @@
 ;; Given two integers, write a function which returns the
 ;; greatest common divisor.
 
-(def __ :tests-will-fail)
+(defn gcd
+  "https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclidean_algorithm"
+  [x y]
+  (loop [a (max x y)
+         b (min x y)]
+    (let [d (mod a b)]
+      (cond
+        (= d 0) b
+        :else (recur b d)))))
 
-(comment
-  
-  )
+(def __ gcd)
 
 (tests
-  (__ 2 4) := 2
-  (__ 10 5) := 5
-  (__ 5 7) := 1
-  (__ 1023 858) := 33)
+ (__ 2 4) := 2
+ (__ 10 5) := 5
+ (__ 5 7) := 1
+ (__ 1023 858) := 33)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/4aa85a5a1d6e6962548e6dc69a02a55e
