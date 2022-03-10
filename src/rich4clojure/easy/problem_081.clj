b622f4c2c597e856781697128f0291f0dc9dc41f
Andrey Miskov <amiskov@gmail.com>
2022-03-10 09:58:59 +0300

    81

--- src/rich4clojure/easy/problem_081.clj
+++ src/rich4clojure/easy/problem_081.clj
@@ -10,18 +10,30 @@
 ;; sets. The intersection is the sub-set of items that
 ;; each set has in common.
 
-(def restricted [intersection])
+(def restricted [clojure.set/intersection])
 
-(def __ :tests-will-fail)
+(def __ (fn [s1 s2]
+          (reduce (fn [acc el]
+                    (if (s2 el)
+                      (conj acc el)
+                      acc)) #{} s1)))
 
 (comment
-  
-  )
+
+  (defn inter [s1 s2]
+    (reduce (fn [acc el]
+              (if (s2 el)
+                (conj acc el)
+                acc)) #{} s1))
+
+  (#{0 1 2 3} 4)
+  (inter #{0 4 2 3} #{2 3 4 5})
+  #_e)
 
 (tests
-  (__ #{0 1 2 3} #{2 3 4 5}) := #{2 3}
-  (__ #{0 1 2} #{3 4 5}) := #{}
-  (__ #{:a :b :c :d} #{:c :e :a :f :d}) := #{:a :c :d})
+ (__ #{0 1 2 3} #{2 3 4 5}) := #{2 3}
+ (__ #{0 1 2} #{3 4 5}) := #{}
+ (__ #{:a :b :c :d} #{:c :e :a :f :d}) := #{:a :c :d})
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/a51be488815f09d5d8e68e93d16f61e8
